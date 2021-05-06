b36b8ceb050ffc19f0f995f6d985eb78234e4959
vstrkv <37896140+vstrkv@users.noreply.github.com>
2021-05-07 00:23:05 +0700

    Delete 2.4.2 directory

--- 2.4.2/src/main/java/web/security/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package web.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-    //пустой класс, использующийся для регистрации модуля в спринг-контейнере
-}
