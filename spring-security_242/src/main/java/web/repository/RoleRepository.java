f64f58362a6428aefaa62de3fa3edfeaef6e15ad
vstrkv <37896140+vstrkv@users.noreply.github.com>
2021-05-08 02:08:31 +0700

    Delete spring-security_242 directory

--- spring-security_242/src/main/java/web/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package web.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import web.model.Role;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role,Integer> {
-}
